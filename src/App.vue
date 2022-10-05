3dc31c07b82ea4134c87d031170ecda200190488
Ester79 <esterramoscuervo@hotmail.com>
2022-10-05 02:52:44 +0200

    Iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,73 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+import "./App.css";
+
+function App() {
+  <template>
+  <div class="mainContainer">
+    <h1 class="titleTable">IronContacts</h1>
+   <table class="tble">
+    <tr class="headerTable">
+      <td>Picture</td>
+      <td>Name</td>
+      <td>Popularity</td>
+    </tr>
+    <tr class="singleColumns" v-for="contact in contacts.slice(0, 5)" :key="contact.id">
+      <td><img class="pictures" :src="contact.pictureUrl" alt=""></td>
+      <td>{{ contact.name }}</td>
+      <td>{{ contact.popularity }}</td>
+    </tr>
+   </table>
+  </div>
 </template>
+<script>
+  import contacts from "./contacts.json";
 
+  export default {
+    data() {
+      return {
+        contacts: contacts,
+
+      };
+    },
+    computed: {
+      getListContacts() {
+        return this.contacts.map((element) => {
+          return {
+            name: element.name,
+            picture: element.pictureUrl,
+            popularity: element.popularity,
+          };
+        });
+      },
+    },
+  }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.pictures{
+  height: 150px;
+  width: auto;
 }
+
+
+.headerTable {
+  font-weight: bold;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.titleTable{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.mainContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.tble{
+  justify-content: center;
+}
+.singleColumns{
+  text-align: center;
+}
+
 </style>
+}
+export default App;
